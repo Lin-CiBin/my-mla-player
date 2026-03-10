f2cb19661ebbf1ed97373beb587b89745852217b
林赐斌 <lincibin@tdx.com.cn>
2026-03-10 16:46:18 +0800

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // 必须设置为 export 才能生成静态文件
-  // 如果你的网址不是根目录（比如有项目名后缀），请解除下面两行的注释并填入项目名
-  // basePath: '/your-repo-name', 
-  // assetPrefix: '/your-repo-name',
+  output: 'export', // 必须
+  basePath: '/my-mla-player', // 关键：匹配你的仓库名
+  assetPrefix: '/my-mla-player', // 关键：确保 CSS/JS 引用路径正确
   images: {
-    unoptimized: true, // 静态导出必须禁用默认的图片优化
+    unoptimized: true, // 静态导出必须禁用图片优化
   },
 };
 
