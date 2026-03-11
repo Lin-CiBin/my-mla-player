beba63fbbb4c3989f84b841d5340d6a9462e6f1b
林赐斌 <lincibin@tdx.com.cn>
2026-03-11 10:19:01 +0800

    up12

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 const nextConfig = {
   output: 'export',      // 必须：启用静态导出
   basePath: '/my-mla-player', // 必须：匹配你的仓库名
+  trailingSlash: true,   // 必须：解决静态路由 404 问题
   images: {
     unoptimized: true,   // 必须：静态导出不支持图片优化
   },
