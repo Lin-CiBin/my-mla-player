cddd081d3ab071bd55e7ab0db4295f4f53ca85be
林赐斌 <lincibin@tdx.com.cn>
2026-03-11 08:57:25 +0800

    updated

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export', // 必须
   basePath: '/my-mla-player', // 关键：匹配你的仓库名
   assetPrefix: '/my-mla-player', // 关键：确保 CSS/JS 引用路径正确
+  trailingSlash: true, 
   images: {
     unoptimized: true, // 静态导出必须禁用图片优化
   },
